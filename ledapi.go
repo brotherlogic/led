64ec7f568b45a33e6d60acfec1dffb0d2e8d385f
Simon Tucker <brotherlogic@gmail.com>
2018-08-15 18:54:15 -0700

    Run display in background

--- ledapi.go
+++ ledapi.go
@@ -10,6 +10,6 @@ import (
 
 // Display shows the text
 func (s *Server) Display(ctx context.Context, req *pb.DisplayRequest) (*pb.DisplayResponse, error) {
-	s.ledwriter.topline(req.GetFullText(), time.Minute)
+	go s.ledwriter.topline(req.GetFullText(), time.Minute)
 	return &pb.DisplayResponse{}, nil
 }
